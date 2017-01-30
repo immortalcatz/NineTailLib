f856e20c5e6e827e1701a8c40b481c41a2647ec4
KitsuneAlex <kitsune-alex@gmx.de>
2017-01-30 23:32:39 +0100

    0.0.22

--- src/main/java/keri/ninetaillib/mod/network/NineTailLibCPH.java
+++ src/main/java/keri/ninetaillib/mod/network/NineTailLibCPH.java
@@ -1,8 +1,12 @@
 package keri.ninetaillib.mod.network;
 
 import codechicken.lib.packet.PacketCustom;
+import keri.ninetaillib.tile.TileEntityBase;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.INetHandlerPlayClient;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class NineTailLibCPH implements PacketCustom.IClientPacketHandler {
 
@@ -10,11 +14,21 @@ public class NineTailLibCPH implements PacketCustom.IClientPacketHandler {
     public void handlePacket(PacketCustom packet, Minecraft minecraft, INetHandlerPlayClient handler) {
         switch(packet.getType()){
             case 1:
+                this.handleTilePacket(packet, minecraft.theWorld, packet.readPos());
+            case 2:
                 this.handleRequestPackage(packet);
                 break;
-            case 2:
+            case 3:
                 this.handleRemovalPackage(packet);
                 break;
+        }
+    }
+
+    private void handleTilePacket(PacketCustom packet, World world, BlockPos pos){
+        TileEntity tile = (TileEntity)world.getTileEntity(pos);
+
+        if(tile instanceof TileEntityBase){
+            ((TileEntityBase)tile).readFromPacket(packet);
         }
     }
 
--- src/main/java/keri/ninetaillib/tile/TileEntityBase.java
+++ src/main/java/keri/ninetaillib/tile/TileEntityBase.java
@@ -1,5 +1,10 @@
 package keri.ninetaillib.tile;
 
+import codechicken.lib.data.MCDataInput;
+import codechicken.lib.data.MCDataOutput;
+import codechicken.lib.packet.ICustomPacketTile;
+import codechicken.lib.packet.PacketCustom;
+import keri.ninetaillib.mod.NineTailLib;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
@@ -10,48 +15,71 @@ import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
-public abstract class TileEntityBase extends TileEntity {
+public abstract class TileEntityBase extends TileEntity implements ICustomPacketTile {
 
     @Override
-    public void readFromNBT(NBTTagCompound tag) {
+    public void readFromNBT(NBTTagCompound tag){
         super.readFromNBT(tag);
         this.readCustomNBT(tag);
     }
 
     @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound tag) {
+    public NBTTagCompound writeToNBT(NBTTagCompound tag){
         super.writeToNBT(tag);
         this.writeCustomNBT(tag);
         return tag;
     }
 
+    public PacketCustom writeToPacketCustom(){
+        PacketCustom packet = new PacketCustom(NineTailLib.INSTANCE, 1);
+        this.writeToPacket(packet);
+        return packet;
+    }
+
+    @Override
+    public void writeToPacket(MCDataOutput packet){
+        NBTTagCompound tag = new NBTTagCompound();
+        this.writeCustomNBT(tag);
+        packet.writeNBTTagCompound(tag);
+    }
+
+    @Override
+    public void readFromPacket(MCDataInput packet) {
+        this.readCustomNBT(packet.readNBTTagCompound());
+        this.notifyUpdate();
+    }
+
     @Nullable
     @Override
     public SPacketUpdateTileEntity getUpdatePacket() {
-        NBTTagCompound tag = new NBTTagCompound();
-        this.writeCustomNBT(tag);
-        return new SPacketUpdateTileEntity(this.getPos(), 3, tag);
+        return this.writeToPacketCustom().toTilePacket(this.getPos());
     }
 
     @Override
     public NBTTagCompound getUpdateTag() {
-        NBTTagCompound tag = super.writeToNBT(new NBTTagCompound());
-        this.writeCustomNBT(tag);
-        return tag;
+        return this.writeToPacketCustom().toNBTTag(super.getUpdateTag());
     }
 
     @Override
     public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
-        this.readCustomNBT(packet.getNbtCompound());
-        IBlockState state = this.worldObj.getBlockState(this.getPos());
-        this.worldObj.notifyBlockUpdate(this.getPos(), state, state, 3);
-        this.worldObj.markBlockRangeForRenderUpdate(this.getPos(), this.getPos());
-        this.worldObj.notifyNeighborsOfStateChange(this.getPos(), this.worldObj.getBlockState(this.getPos()).getBlock());
+        this.readFromPacket(PacketCustom.fromTilePacket(packet));
+    }
+
+    @Override
+    public void handleUpdateTag(NBTTagCompound tag) {
+        this.readFromPacket(PacketCustom.fromNBTTag(tag));
     }
 
     @Override
     public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
         return oldState.getBlock() != newSate.getBlock();
+    }
+
+    private void notifyUpdate(){
+        IBlockState state = this.worldObj.getBlockState(this.getPos());
+        this.worldObj.notifyBlockUpdate(this.getPos(), state, state, 3);
+        this.worldObj.notifyNeighborsOfStateChange(this.getPos(), this.worldObj.getBlockState(this.getPos()).getBlock());
+        this.worldObj.markBlockRangeForRenderUpdate(this.getPos(), this.getPos());
     }
 
     public abstract void readCustomNBT(NBTTagCompound tag);
