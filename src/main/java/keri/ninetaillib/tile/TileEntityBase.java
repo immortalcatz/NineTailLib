6b19f09782dbea6e61e4b26e77daf51c2183ff42
KitsuneAlex <kitsune-alex@gmx.de>
2017-01-30 22:05:55 +0100

    0.0.20

--- src/main/java/keri/ninetaillib/tile/TileEntityBase.java
+++ src/main/java/keri/ninetaillib/tile/TileEntityBase.java
@@ -1,10 +1,6 @@
 package keri.ninetaillib.tile;
 
-import codechicken.lib.data.MCDataInput;
-import codechicken.lib.data.MCDataOutput;
-import codechicken.lib.packet.ICustomPacketTile;
-import codechicken.lib.packet.PacketCustom;
-import keri.ninetaillib.mod.NineTailLib;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
@@ -12,52 +8,28 @@ import net.minecraft.tileentity.TileEntity;
 
 import javax.annotation.Nullable;
 
-public class TileEntityBase extends TileEntity implements ICustomPacketTile {
+public class TileEntityBase extends TileEntity {
+
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        return this.writeToNBT(new NBTTagCompound());
+    }
 
     @Nullable
     @Override
     public SPacketUpdateTileEntity getUpdatePacket() {
-        return this.writeToPacketCustom().toTilePacket(this.getPos());
-    }
-
-    @Override
-    public NBTTagCompound getUpdateTag() {
-        return this.writeToPacketCustom().toNBTTag(super.getUpdateTag());
+        NBTTagCompound tag = new NBTTagCompound();
+        this.writeToNBT(tag);
+        return new SPacketUpdateTileEntity(this.getPos(), 0, tag);
     }
 
     @Override
     public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
-        this.readFromPacket(PacketCustom.fromTilePacket(packet));
-    }
-
-    @Override
-    public void handleUpdateTag(NBTTagCompound tag) {
-        this.readFromPacket(PacketCustom.fromNBTTag(tag));
-    }
-
-    protected void sendUpdatePacket(){
-        this.writeToPacketCustom().sendToChunk(this.worldObj, getPos().getX() >> 4, getPos().getZ() >> 4);
-    }
-
-    public PacketCustom writeToPacketCustom(){
-        PacketCustom packet = new PacketCustom(NineTailLib.INSTANCE, 1);
-        this.writeToPacket(packet);
-        return packet;
-    }
-
-    @Override
-    public void writeToPacket(MCDataOutput data) {
-        NBTTagCompound tag = new NBTTagCompound();
-        this.writeToNBT(tag);
-        data.writeNBTTagCompound(tag);
-    }
-
-    @Override
-    public void readFromPacket(MCDataInput data) {
-        this.readFromNBT(data.readNBTTagCompound());
-        this.markDirty();
+        super.onDataPacket(net, packet);
+        this.readFromNBT(packet.getNbtCompound());
+        IBlockState state = getWorld().getBlockState(getPos());
+        this.worldObj.notifyBlockUpdate(getPos(), state, state, 3);
         this.worldObj.notifyNeighborsOfStateChange(this.getPos(), this.worldObj.getBlockState(this.getPos()).getBlock());
-        this.worldObj.markBlockRangeForRenderUpdate(this.getPos(), this.getPos());
     }
 
 }
