fe8b622f32daeeaf23b5aca833dc66828eb1489a
KitsuneAlex <kitsune-alex@gmx.de>
2017-03-17 20:02:23 +0100

    Increment build number

--- src/main/java/keri/ninetaillib/internal/util/ModPrefs.java
+++ src/main/java/keri/ninetaillib/internal/util/ModPrefs.java
@@ -4,7 +4,7 @@ public class ModPrefs {
 
     public static final String MODID = "ninetaillib";
     public static final String NAME = "NineTailLib";
-    public static final String VERSION = "0.0.48";
+    public static final String VERSION = "0.0.49";
     public static final String ACC_MC = "1.10.2";
     public static final String DEPS = "";
     public static final String CSIDE = "keri.ninetaillib.internal.proxy.ClientProxy";
