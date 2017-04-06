385f51f588a03f5a5fe5fa0816d22122b5b4da5b
KitsuneAlex <kitsune-alex@gmx.de>
2017-04-06 19:32:02 +0200

    0.0.66
    - FINALLY finally fix for ItemArmorHelper >.>
    - Removed BlockOrientation
    - Removed BLOCK_ORIENTATION property

--- src/main/java/keri/ninetaillib/property/CommonProperties.java
+++ src/main/java/keri/ninetaillib/property/CommonProperties.java
@@ -1,39 +1,10 @@
 package keri.ninetaillib.property;
 
 import net.minecraft.block.properties.PropertyEnum;
-import net.minecraftforge.common.property.IUnlistedProperty;
 
 public class CommonProperties {
 
     public static final PropertyEnum<EnumDyeColor> DYE_COLOR = PropertyEnum.create("type", EnumDyeColor.class);
     public static final NBTTagProperty NBT_TAG_PROPERTY = new NBTTagProperty();
-
-    /**
-     * Will be removed in the next updates !
-     */
-    @Deprecated
-    public static final IUnlistedProperty BLOCK_ORIENTATION = new IUnlistedProperty<BlockOrientation>(){
-
-        @Override
-        public String getName() {
-            return "block_orientation";
-        }
-
-        @Override
-        public boolean isValid(BlockOrientation value) {
-            return value.getOrientation() != null;
-        }
-
-        @Override
-        public Class<BlockOrientation> getType() {
-            return BlockOrientation.class;
-        }
-
-        @Override
-        public String valueToString(BlockOrientation value) {
-            return value.getOrientation().getName();
-        }
-
-    };
 
 }
