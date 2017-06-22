d40b368e3d0e9d6d30770489c58c92c8da5cc31d
KitsuneAlex <kitsune-alex@gmx.de>
2017-06-22 19:07:27 +0200

    1.6.131

--- src/main/java/keri/ninetaillib/mod/playereffect/PlayerEffects.java
+++ src/main/java/keri/ninetaillib/mod/playereffect/PlayerEffects.java
@@ -11,8 +11,6 @@ import java.util.UUID;
 public class PlayerEffects {
 
     public static void preInit(){
-        //KitsuneAlex
-        PlayerEffectHandler.INSTANCE.register(UUID.fromString("b2ac8c03-d994-4805-9e0f-57fede63c04d"), new PlayerEffectWolf());
         //AtomicBlom
         PlayerEffectHandler.INSTANCE.register(UUID.fromString("58d506e2-7ee7-4774-8b22-c7a57eda488b"), new PlayerEffectSheep());
         //LiteWolf101
