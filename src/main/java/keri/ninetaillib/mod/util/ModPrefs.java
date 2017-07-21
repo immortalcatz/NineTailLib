c8ad50ce5289efd339ee59a003f84a28d1d34677
KitsuneAlex <kitsune-alex@gmx.de>
2017-07-21 15:57:04 +0200

    1.6.139

--- src/main/java/keri/ninetaillib/mod/util/ModPrefs.java
+++ src/main/java/keri/ninetaillib/mod/util/ModPrefs.java
@@ -10,7 +10,7 @@ public class ModPrefs {
 
     public static final String MODID = "ninetaillib";
     public static final String NAME = "NineTailLib";
-    public static final String VERSION = "1.6.138";
+    public static final String VERSION = "1.6.139";
     public static final String VERSION_CORE = "0.0.4";
     public static final String DEPS = "";
     public static final String ACC_MC = "1.11.2";
